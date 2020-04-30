f635a7bfc6bbd972a3fc5dca3b2f179a33738c9c
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-04-30 14:32:43 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -10,6 +10,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 version = "2019.2"
 
 project {
+    vcsRoot(Discovery)
 
     features {
         dockerRegistry {
@@ -116,8 +117,5 @@ object Discovery_DiscoveryBuild : BuildType({
 object Discovery : GitVcsRoot({
     name = "Discovery"
     url = "https://github.com/DevOps-DP-185/Discovery.git"
-    authMethod = password {
-        userName = "ArtemKulish"
-        password = "credentialsJSON:91a788d6-72b3-405f-a9df-03389f20d48c"
     }
 })

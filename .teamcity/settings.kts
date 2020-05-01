e0bc0de085a50b4385c93bd6424a82b74f880a7b
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-05-01 12:40:41 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -12,7 +12,16 @@ version = "2019.2"
 project {
     vcsRoot(Something)
     vcsRoot(Discovery)
-    subProject(Test)       
+    subProject(Test)     
+    features {
+        dockerRegistry {
+            id = "Demo_4"
+            name = "Docker Registry"
+            url = "https://docker.io"
+            userName = "artemkulish"
+            password = "credentialsJSON:91a788d6-72b3-405f-a9df-03389f20d48c"
+        }
+    }
 }
 
 object Test : Project({

e6884af95ddbf72b0acf1b11f57f222b1595bd64
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-05-01 12:26:12 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -12,11 +12,19 @@ project {
 object Something : GitVcsRoot({
     name = "Something"
     url = "https://github.com/DevOps-DP-185/Ansible.git"
+    authMethod = password {
+        userName = "ArtemKulish"
+        password = "credentialsJSON:91a788d6-72b3-405f-a9df-03389f20d48c"
+    }
 })
 
 object Discovery : GitVcsRoot({
     name = "Discovery"
     url = "https://github.com/DevOps-DP-185/Discovery.git"
+    authMethod = password {
+        userName = "ArtemKulish"
+        password = "credentialsJSON:91a788d6-72b3-405f-a9df-03389f20d48c"
+    }
 })
 
 object Gateway : GitVcsRoot({

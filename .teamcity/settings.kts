47251fbf3ed7759c8a6feb4a06ab276ecdd69a99
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-05-01 14:08:31 +0300

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -40,14 +40,12 @@ object Test_1 : Project({
     name = "Test_1"
     
     vcsRoot(Payment)
-    buildType(Payment)
 })
 
 object Test_2 : Project({
     name = "Test_2"
     
     vcsRoot(Discovery)
-    buildType(Discovery)
 })
 
 /******************************************
@@ -84,96 +82,6 @@ object Test_Build : BuildType({
         dockerCommand {
             commandType = push {
                 namesAndTags = "artemkulish/demo4:gateway"
-            }
-            param("dockerfile.path", "Dockerfile")
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {
-        dockerSupport {
-            loginToRegistry = on {
-                dockerRegistryId = "Demo_4"
-            }
-        }
-    }
-})
-
-object Payment : BuildType({
-    name = "Build"
-
-    vcs {
-        root(Payment)
-    }
-
-    steps {
-        maven {
-            goals = "clean package"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            localRepoScope = MavenBuildStep.RepositoryScope.MAVEN_DEFAULT
-            jdkHome = "%env.JDK_11%"
-        }
-        dockerCommand {
-            commandType = build {
-                source = file {
-                    path = "Dockerfile"
-                }
-                namesAndTags = "artemkulish/demo4:payment"
-            }
-            param("dockerImage.platform", "linux")
-        }
-        dockerCommand {
-            commandType = push {
-                namesAndTags = "artemkulish/demo4:payment"
-            }
-            param("dockerfile.path", "Dockerfile")
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-
-    features {
-        dockerSupport {
-            loginToRegistry = on {
-                dockerRegistryId = "Demo_4"
-            }
-        }
-    }
-})
-
-object Discovery : BuildType({
-    name = "Build"
-
-    vcs {
-        root(Discovery)
-    }
-
-    steps {
-        maven {
-            goals = "clean package"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            localRepoScope = MavenBuildStep.RepositoryScope.MAVEN_DEFAULT
-            jdkHome = "%env.JDK_11%"
-        }
-        dockerCommand {
-            commandType = build {
-                source = file {
-                    path = "Dockerfile"
-                }
-                namesAndTags = "artemkulish/demo4:discovery"
-            }
-            param("dockerImage.platform", "linux")
-        }
-        dockerCommand {
-            commandType = push {
-                namesAndTags = "artemkulish/demo4:discovery"
             }
             param("dockerfile.path", "Dockerfile")
         }

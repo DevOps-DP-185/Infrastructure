94ecfeb25e0ab0115bc1149e96318258f1af9168
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-05-02 15:54:27 +0300

    Update Project.kt

--- .teamcity/Discovery/Project.kt
+++ .teamcity/Discovery/Project.kt
@@ -9,7 +9,7 @@ object Project : Project({
     id("Discovery")
     name = "Discovery"
 
-    vcsRoot(Discovery_vcs)
+    vcsRoot(Discovery)
 
     buildType(Discovery_Deploy)
     buildType(Discovery_Build)

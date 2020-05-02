f6b0426157204e56842dc0c847d56e6ba6f06be5
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-05-02 15:37:40 +0300

    Update Project.kt

--- .teamcity/_Self/Project.kt
+++ .teamcity/_Self/Project.kt
@@ -5,10 +5,10 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.dockerRegistry
 
-project {
-    subProject(Discovery) 
-    subProject(Payment)
-    subProject(Gateway)
+object Project : Project({
+    subProject(Discovery.Project) 
+    subProject(Payment.Project)
+    subProject(Gateway.Project)
     
     features {
         dockerRegistry {
@@ -19,4 +19,4 @@ project {
             password = "credentialsJSON:91a788d6-72b3-405f-a9df-03389f20d48c"
         }
     }
-}
+})

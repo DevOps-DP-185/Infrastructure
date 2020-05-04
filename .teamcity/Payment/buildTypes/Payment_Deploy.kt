7bf7e7b4e8af20c1c088f671f9cbde75ab40b468
Artem Kulish <57228865+Artemkulish@users.noreply.github.com>
2020-05-04 13:41:03 +0300

    Update Payment_Deploy.kt

--- .teamcity/Payment/buildTypes/Payment_Deploy.kt
+++ .teamcity/Payment/buildTypes/Payment_Deploy.kt
@@ -4,7 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 object Payment_Deploy : BuildType({
-    name = "Payment"
+    name = "Deploy"
 
     enablePersonalBuilds = false
     type = BuildTypeSettings.Type.DEPLOYMENT
@@ -18,7 +18,7 @@ object Payment_Deploy : BuildType({
         step {
             type = "ssh-exec-runner"
             param("jetbrains.buildServer.sshexec.command", "ls")
-            param("jetbrains.buildServer.deployer.targetUrl", "35.184.252.223")
+            param("jetbrains.buildServer.deployer.targetUrl", "10.128.0.9")
             param("jetbrains.buildServer.sshexec.authMethod", "DEFAULT_KEY")
             param("jetbrains.buildServer.sshexec.keyFile", "/home/artemkulish123/")
         }

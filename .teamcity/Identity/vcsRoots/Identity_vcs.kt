9c2fe7b2988cebd61c5383301649394f959ecd59
artemkulish123 <artemkulish123@gmail.com>
2020-05-06 00:39:19 +0300

    VCS hook is added

--- .teamcity/Identity/vcsRoots/Identity_vcs.kt
+++ .teamcity/Identity/vcsRoots/Identity_vcs.kt
@@ -1,11 +1,11 @@
-package Vehicle.vcsRoots
+package Identity.vcsRoots
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-object Vehicle_vcs : GitVcsRoot({
-    name = "Vehicle_vcs"
-    url = "https://github.com/DevOps-DP-185/Vehicle.git"
+object Identity_vcs : GitVcsRoot({
+    name = "Identity_vcs"
+    url = "https://github.com/DevOps-DP-185/Identity.git"
     authMethod = password {
         userName = "ArtemKulish"
         password = "credentialsJSON:91a788d6-72b3-405f-a9df-03389f20d48c"

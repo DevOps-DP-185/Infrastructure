f29416024abd5f71698b4ea84580ff26f4bcd6ba
DevOps-source <61282785+DevOps-source@users.noreply.github.com>
2020-05-20 10:21:57 +0300

    scale

---
+++ terraform/VPN_DB/modules/vm_scale_set/main.tf
@@ -0,0 +1,70 @@
+#az vmss create  -n MyVmss \
+# -g rtytr \
+#--admin-username vitaliy \
+# --image UbuntuLTS /
+# --specialized/
+#--ssh-key-values /home/grishenkovitali/.ssh/id_rsa.pub \
+#--subnet sub \
+#--vm-sku Standard_F2 \
+#--vnet-name mynet \
+#--app-gateway gatetesr \
+#--instance-count 1 \
+
+resource "null_resource" "create_vmss" {
+  provisioner "local-exec" {
+  command = "az vmss create  -n MyVmss -g demo \
+--image /subscriptions/92c6b1c1-2921-4a64-b4dc-dd0f92882bf6/resourceGroups/demo/providers/Microsoft.Compute/images/image \
+--instance-count 1 \
+--vm-sku Standard_B1s \
+--vnet-name AGVNET \
+--subnet Sub \
+--app-gateway example-appgateway"
+--admin-username grishenkovitali
+#--ssh-key-values /home/grishenkovitali/key.pub
+
+
+# "az vm generalize --resource-group '${var.group_name}' --name '${azurerm_linux_virtual_machine.example.name}'"
+
+  }
+}
+
+resource "null_resource" "autoscale_create" {
+  provisioner "local-exec" {
+  command = "az monitor autoscale create -g demo --resource MyVmss \
+--resource-type Microsoft.Compute/virtualMachineScaleSets \
+--name autoscale \
+--min-count 1 \
+--max-count 2 \
+--count 1"
+
+  }
+ depends_on = [
+    null_resource.create_vmss
+  ]
+}
+
+resource "null_resource" "rule_increase" {
+  provisioner "local-exec" {
+  command = "az monitor autoscale rule create \
+--resource-group demo \  
+--autoscale-name autoscale \  
+--condition "Percentage CPU > 70 avg 1m" \
+--scale out 1"
+
+  }
+ depends_on = [
+    null_resource.autoscale_create
+  ]
+}
+
+resource "null_resource" "rule_decrease" {
+  provisioner "local-exec" {
+  command = "az monitor autoscale rule create --resource-group demo \
+--autoscale-name autoscale \ 
+--condition "Percentage CPU < 25 avg 1m" \
+--scale in 1"
+  }
+ depends_on = [
+    null_resource.autoscale_create
+  ]
+}

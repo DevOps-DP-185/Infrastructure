5a758c6aae0c1edce3f18bc8b89bcff45383ab2a
DevOps-source <61282785+DevOps-source@users.noreply.github.com>
2020-05-20 18:27:23 +0300

    Update main.tf

--- terraform/VPN_DB/modules/vm_scale_set/main.tf
+++ terraform/VPN_DB/modules/vm_scale_set/main.tf
@@ -1,22 +1,22 @@
-resource "null_resource" "create_vmss1" {
+resource "null_resource" "create_vmss1-1" {
   provisioner "local-exec" {
-  command = "az vmss create  -n MyVmss -g demo --image '${var.image_id}' --instance-count 1 --vm-sku Standard_D2s_v3 --vnet-name AGVNET --subnet Sub --app-gateway example-appgateway --admin-username artemkulish123"
+  command = "az vmss create  -n MyVmss -g group --image  /subscriptions/92c6b1c1-2921-4a64-b4dc-dd0f92882bf6/resourceGroups/group/providers/Microsoft.Compute/images/image --instance-count 1 --vm-sku Standard_D2s_v3 --vnet-name AGVNET --subnet Sub --app-gateway example-appgateway --admin-username artemkulish123"
   }
 }
 
 resource "null_resource" "autoscale_create1" {
   provisioner "local-exec" {
-  command = "az monitor autoscale create -g demo --resource MyVmss --resource-type Microsoft.Compute/virtualMachineScaleSets --name autoscale --min-count 1 --max-count 2 --count 1"
+  command = "az monitor autoscale create -g group --resource MyVmss --resource-type Microsoft.Compute/virtualMachineScaleSets --name autoscale --min-count 1 --max-count 2 --count 1"
 
   }
  depends_on = [
-    null_resource.create_vmss1
+    null_resource.create_vmss1-1
   ]
 }
 
 resource "null_resource" "rule_increase1" {
   provisioner "local-exec" {
-  command = "az monitor autoscale rule create --resource-group demo  --autoscale-name autoscale   --condition 'Percentage CPU > 70 avg 1m'  --scale out 1"
+  command = "az monitor autoscale rule create --resource-group group  --autoscale-name autoscale   --condition 'Percentage CPU > 70 avg 1m'  --scale out 1"
 
   }
  depends_on = [
@@ -26,7 +26,7 @@ resource "null_resource" "rule_increase1" {
 
 resource "null_resource" "rule_decrease1" {
   provisioner "local-exec" {
-  command = "az monitor autoscale rule create --resource-group demo --autoscale-name autoscale --condition 'Percentage CPU < 25 avg 1m' --scale in 1"
+  command = "az monitor autoscale rule create --resource-group group --autoscale-name autoscale --condition 'Percentage CPU < 25 avg 1m' --scale in 1"
   }
  depends_on = [
     null_resource.autoscale_create1

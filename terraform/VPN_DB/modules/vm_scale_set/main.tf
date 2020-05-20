174b6d1735b0c51b792c99dc348b43cf15ff0e1a
DevOps-source <61282785+DevOps-source@users.noreply.github.com>
2020-05-20 18:37:39 +0300

    Update main.tf

--- terraform/VPN_DB/modules/vm_scale_set/main.tf
+++ terraform/VPN_DB/modules/vm_scale_set/main.tf
@@ -1,8 +1,11 @@
-resource "null_resource" "create_vmss1-1" {
+resource "null_resource" "create_vmss101" {
   provisioner "local-exec" {
   command = "az vmss create  -n MyVmss -g group --image  /subscriptions/92c6b1c1-2921-4a64-b4dc-dd0f92882bf6/resourceGroups/group/providers/Microsoft.Compute/images/image --instance-count 1 --vm-sku Standard_D2s_v3 --vnet-name AGVNET --subnet Sub --app-gateway example-appgateway --admin-username artemkulish123"
   }
-}
+ depends_on = [
+    var.image_id
+  ]
+  }
 
 resource "null_resource" "autoscale_create1" {
   provisioner "local-exec" {
@@ -10,7 +13,7 @@ resource "null_resource" "autoscale_create1" {
 
   }
  depends_on = [
-    null_resource.create_vmss1-1
+    null_resource.create_vmss101
   ]
 }
 

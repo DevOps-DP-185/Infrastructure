38d898953f42eeea6f50e983535ff35ada2462b5
DevOps-source <61282785+DevOps-source@users.noreply.github.com>
2020-05-20 19:41:29 +0300

    Update output.tf

--- terraform/VPN_DB/modules/azure_vm/output.tf
+++ terraform/VPN_DB/modules/azure_vm/output.tf
@@ -2,8 +2,8 @@ output "private_ip_address" {
     value = azurerm_linux_virtual_machine.example.private_ip_address
 }
 output "image_id" {
-    value = "azurerm_image.example.id"
+    value = azurerm_image.example.id
     }
-output "delete_vm" {
-    value = "null_resource.test222"
-}
+#output "delete_vm" {
+#    value = null_resource.test222
+#}

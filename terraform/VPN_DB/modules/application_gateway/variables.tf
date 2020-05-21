5c8bf6db294fdcd721d331cef1f694d3285db0d4
DevOps-source <61282785+DevOps-source@users.noreply.github.com>
2020-05-21 13:49:17 +0300

    Update variables.tf

--- terraform/VPN_DB/modules/application_gateway/variables.tf
+++ terraform/VPN_DB/modules/application_gateway/variables.tf
@@ -1,10 +1,9 @@
 variable "group_location" {}
 variable "group_name" {}
 variable "network" {}
-#variable "private_ip_address" {}
-#variable "ssl_policy_name" {
-#  description = "SSL policy name"
-#  default     = "AppGwSslPolicy20170401S"
-#}
-#variable "certificate_content" {}
-#variable "certificate_passphrase" {}
+variable "ssl_policy_name" {
+  description = "SSL policy name"
+  default     = "AppGwSslPolicy20170401S"
+}
+variable "certificate_content" {}
+variable "certificate_passphrase" {}
